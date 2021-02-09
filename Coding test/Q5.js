ac530755071363fc8bea6dc8fb5a6a54a83e30e9
Vishal Yadav <53209517+vishalyadav10@users.noreply.github.com>
2021-02-09 19:43:35 +0530

    Create Q5.js
    Support you have a html tag like this 
    <div>Javascript Developer</div>
    Write a small html code to add a class on this div. Add a rounded border box on this div and also there should be a gap of 5px between the text and the rounded box and the text.

---
+++ Coding test/Q5.js
@@ -0,0 +1,16 @@
+HTML
+  <div class='container'>
+  <h1> Javascript Developer</h1>
+    </div>
+CSS
+.container{
+display: flex;
+justify-content: center;
+align-items: center;
+height: 200px;
+width: 200px;
+background:blue;
+border-radius: 10%;
+padding:5px;
+}
+
